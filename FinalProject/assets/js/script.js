7f2cbb0b28461889a8cb368189dddfda8b6961a3
omirkhanov06 <65611438+omirkhanov06@users.noreply.github.com>
2023-11-25 11:00:19 +0600

    Add files via upload

---
+++ FinalProject/assets/js/script.js
@@ -0,0 +1,138 @@
+'use strict';
+
+
+
+/**
+ * Add event listener on multiple elements
+ */
+
+const addEventOnElements = function (elements, eventType, callback) {
+  for (let i = 0, len = elements.length; i < len; i++) {
+    elements[i].addEventListener(eventType, callback);
+  }
+}
+
+
+
+/**
+ * MOBILE NAVBAR TOGGLER
+ */
+
+const navbar = document.querySelector("[data-navbar]");
+const navTogglers = document.querySelectorAll("[data-nav-toggler]");
+
+const toggleNav = () => {
+  navbar.classList.toggle("active");
+  document.body.classList.toggle("nav-active");
+}
+
+addEventOnElements(navTogglers, "click", toggleNav);
+
+
+
+/**
+ * HEADER ANIMATION
+ * When scrolled donw to 100px header will be active
+ */
+
+const header = document.querySelector("[data-header]");
+const backTopBtn = document.querySelector("[data-back-top-btn]");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 100) {
+    header.classList.add("active");
+    backTopBtn.classList.add("active");
+  } else {
+    header.classList.remove("active");
+    backTopBtn.classList.remove("active");
+  }
+});
+
+
+
+/**
+ * SLIDER
+ */
+
+const slider = document.querySelector("[data-slider]");
+const sliderContainer = document.querySelector("[data-slider-container]");
+const sliderPrevBtn = document.querySelector("[data-slider-prev]");
+const sliderNextBtn = document.querySelector("[data-slider-next]");
+
+let totalSliderVisibleItems = Number(getComputedStyle(slider).getPropertyValue("--slider-items"));
+let totalSlidableItems = sliderContainer.childElementCount - totalSliderVisibleItems;
+
+let currentSlidePos = 0;
+
+const moveSliderItem = function () {
+  sliderContainer.style.transform = `translateX(-${sliderContainer.children[currentSlidePos].offsetLeft}px)`;
+}
+
+/**
+ * NEXT SLIDE
+ */
+
+const slideNext = function () {
+  const slideEnd = currentSlidePos >= totalSlidableItems;
+
+  if (slideEnd) {
+    currentSlidePos = 0;
+  } else {
+    currentSlidePos++;
+  }
+
+  moveSliderItem();
+}
+
+sliderNextBtn.addEventListener("click", slideNext);
+
+/**
+ * PREVIOUS SLIDE
+ */
+
+const slidePrev = function () {
+  if (currentSlidePos <= 0) {
+    currentSlidePos = totalSlidableItems;
+  } else {
+    currentSlidePos--;
+  }
+
+  moveSliderItem();
+}
+
+sliderPrevBtn.addEventListener("click", slidePrev);
+
+/**
+ * RESPONSIVE
+ */
+window.addEventListener("resize", function () {
+  totalSliderVisibleItems = Number(getComputedStyle(slider).getPropertyValue("--slider-items"));
+  totalSlidableItems = sliderContainer.childElementCount - totalSliderVisibleItems;
+
+  moveSliderItem();
+});
+
+
+const quizForm = document.getElementById('quiz-form');
+    quizForm.addEventListener('submit', function(event) {
+      event.preventDefault();
+
+      const answers = ['a', 'd', 'b', 'a', 'c'];
+      const submittedAnswers = [
+        quizForm.question1.value,
+        quizForm.question2.value,
+        quizForm.question3.value,
+        quizForm.question4.value,
+        quizForm.question5.value,
+      ];
+
+      let score = 0;
+      for (let i = 0; i < answers.length; i++) {
+        if (submittedAnswers[i] === answers[i]) {
+          score++;
+        }
+      }
+
+      const resultDisplay = document.getElementById('result');
+      resultDisplay.textContent = `You scored ${score} out of ${answers.length}!`;
+    });
